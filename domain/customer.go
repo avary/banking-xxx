251fced81d2c656f5105634edd4313b4e9e579a5
Ashef <asheftishad@gmail.com>
2022-04-03 16:32:59 +0600

    Starting with Domain. Created customer type and planned intended core functions of it.

---
+++ domain/customer.go
@@ -0,0 +1,22 @@
+package domain
+
+import "github.com/ashtishad/banking/lib"
+
+type Customer struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	City        string `json:"city"`
+	Zipcode     string `json:"zipcode"`
+	DateOfBirth string `json:"date_of_birth"`
+	Status      string `json:"status"`
+}
+
+// CustomerRepository is a SECONDARY PORT on Hexagonal architecture
+type CustomerRepository interface {
+	FindById(id string) (*Customer, *lib.RestErr)
+	FindByStatus(status string) ([]Customer, *lib.RestErr)
+	FindByName(name string) (*Customer, *lib.RestErr)
+	Create(customer *Customer) (*Customer, *lib.RestErr)
+	Update(customer *Customer) (*Customer, *lib.RestErr)
+	Delete(id string) *lib.RestErr
+}
