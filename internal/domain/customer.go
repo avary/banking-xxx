f49d6c8ea877e78e8fa7702d46b69c20cfcdbe03
Ashef <asheftishad@gmail.com>
2022-04-05 03:54:56 +0600

    Created customer dto response.

--- internal/domain/customer.go
+++ internal/domain/customer.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"github.com/ashtishad/banking/internal/dto"
 	"github.com/ashtishad/banking/internal/lib"
 )
 
@@ -22,4 +23,24 @@ type CustomerRepository interface {
 	//Create(customer *Customer) (*Customer, lib.RestErr)
 	//Update(customer *Customer) (*Customer, lib.RestErr)
 	//Delete(id string) lib.RestErr
+}
+
+// statusAsText returns 1= active, 0= inactive
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Customer) ToCustomerResponse() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.statusAsText(),
+	}
 }
